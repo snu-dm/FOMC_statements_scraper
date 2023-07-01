676a11e3da4dd911f0573e42299bbf39c8e820a5
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2023-07-01 20:31:23 +0900

    commit

--- schemas.py
+++ schemas.py
@@ -17,33 +17,6 @@ from sqlalchemy.sql.schema import MetaData
 
 metadata = MetaData()
 
-
-stocks = Table(
-    'stocks', metadata,
-    Column('stockid', Integer, primary_key=True, autoincrement=True),
-    Column('path', String, nullable=False),
-    Column('name', String, nullable=False),
-    Column('code', String),
-    Column('year', Integer),
-    Column('quarter', Integer),
-    Column('itemtype', String),
-    Column('itemindex', String),
-    Column('extension', String),
-    Column('created', DateTime, default=datetime.datetime.utcnow),
-    Column('lastmodified', DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow),
-)
-
-minutes = Table(
-    'minutes', metadata,
-    Column('minuteid', Integer, primary_key=True, autoincrement=True),
-    Column('organization', String),
-    Column('path', String, nullable=False),
-    Column('date', DateTime),
-    Column('extension', String),
-    Column('created', DateTime, default=datetime.datetime.utcnow),
-    Column('lastmodified', DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
-)
-
 statements = Table(
     'statements', metadata,
     Column('statementid', Integer, primary_key=True, autoincrement=True),
